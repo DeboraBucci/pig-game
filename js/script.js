22c972916015418b53ff513b2e9a8cbf99d0cff3
Débora <dbucci@protonmail.com>
2021-05-28 18:27:14 -0300

    Some js functionality added: rolling dice btn now gives a random number between 1 and 6. Also that same number is displayed in the image dice.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+// SELECTING ELEMENTS
+const playerOne = document.querySelector(".player--1");
+const playerTwo = document.querySelector(".player--2");
+
+const scoreOne = document.getElementById("#score--1");
+const scoreTwo = document.getElementById("#score--2");
+
+const currentOne = document.getElementById("#current--1");
+const currentTwo = document.getElementById("#current--2");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// VARIABLES :
+let playing, activePlayer, currentScore, scores;
+
+// STARTING CONDITIONS :
+diceEl.classList.add("hidden");
+
+// ROLLING DICE :
+btnRoll.addEventListener("click", function () {
+  // Generate random number between 1 and 6 :
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // Display Dice :
+  diceEl.classList.remove("hidden");
+  diceEl.src = `/images/dice-${dice}.png`;
+
+  // CHeck if diceNum === 1 :
+  if (dice !== 1) {
+    // Add to current score :
+  } else {
+  }
+});
